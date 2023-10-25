2dc3cd57cae05b2abc7ae80c5a72a5a27fd62c7e
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2023-10-25 09:08:25 +0600

    added format on save feature

--- lua/nahian/plugins/lsp/null_ls.lua
+++ lua/nahian/plugins/lsp/null_ls.lua
@@ -5,11 +5,25 @@ return {
 		local null_ls = require("null-ls")
 		local formatting = null_ls.builtins.formatting
 		local diagnos = null_ls.builtins.diagnostics
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		null_ls.setup({
 			sources = {
 				formatting.black,
 				diagnos.flake8,
 			},
+			-- format on save
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end
 		})
-	end,
+	end
 }
