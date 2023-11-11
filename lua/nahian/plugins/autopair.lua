98f7b53ca5d04733c08f4d549f9b01056bb09190
lemonadeforlife <nahianlabiblimon44@gmail.com>
2023-11-11 23:08:13 +0600

    Fixed auto pairing

--- lua/nahian/plugins/autopair.lua
+++ lua/nahian/plugins/autopair.lua
@@ -1,13 +1,8 @@
 return {
 	'windwp/nvim-autopairs',
 	event = "InsertEnter",
-	opts = {}, -- this is equalent to setup({}) function
-	config = function()
-		local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-		local cmp = require('cmp')
-		cmp.event:on(
-			'confirm_done',
-			cmp_autopairs.on_confirm_done()
-		)
-	end
+    config = function ()
+        local npairs = require('nvim-autopairs')
+        npairs.setup { map_cr = true }
+    end
 }
