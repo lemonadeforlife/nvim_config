af109618a61a4f75c8ba9d18b4c0a8507fdbbf0f
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2023-11-12 00:50:20 +0600

    Fixed Folding Issue

--- lua/nahian/plugins/folding.lua
+++ lua/nahian/plugins/folding.lua
@@ -2,6 +2,13 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = 'kevinhwang91/promise-async',
     config = function ()
+        --folding
+
+        vim.o.foldcolumn = '1' -- '0' is not bad
+        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities.textDocument.foldingRange = {
             dynamicRegistration = false,
@@ -14,6 +21,9 @@ return {
                 -- you can add other fields for setting up lsp server in this table
             })
         end
-        require('ufo').setup()
+        require('ufo').setup {}
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
     end
 }
