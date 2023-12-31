c279dd7f64dfc1a75822862a3e73f9d69afe9d75
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2023-12-31 17:50:33 +0600

    [Enhancment] LSP, AutoCompletion & Linting
    Added Neodiv in order supress and handle lua language.

--- lua/nahian/plugins/lsp/lspconfig.lua
+++ lua/nahian/plugins/lsp/lspconfig.lua
@@ -2,10 +2,12 @@ return {
     "neovim/nvim-lspconfig",
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
+        "folke/neodev.nvim",
         "hrsh7th/cmp-nvim-lsp",
         { "antosha417/nvim-lsp-file-operations", config = true },
     },
     config = function()
+        require('neodev').setup {}
         local lspconfig = require('lspconfig')
         local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
@@ -57,36 +59,10 @@ return {
 
         -- used to enable autocompletion (assign to every lsp server config)
         local cap = cmp_nvim_lsp.default_capabilities()
-        lspconfig["lua_ls"].setup {
-            capabilities = cap,
-            settings = {
-                Lua = {
-                  runtime = {
-                    -- Tell the language server which version of Lua you're using
-                    -- (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT',
-                  },
-                  diagnostics = {
-                    -- Get the language server to recognize the `vim` global
-                    globals = {
-                      'vim',
-                      'require'
-                    },
-                  },
-                  workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = vim.api.nvim_get_runtime_file("", true),
-                  },
-                  -- Do not send telemetry data containing a randomized but unique identifier
-                  telemetry = {
-                    enable = false,
-                  },
-                },
-            },
-        }
+        lspconfig["lua_ls"].setup {}
         lspconfig["pyright"].setup {
             on_attach = on_attach,
-            capabilities = cap
+            capabilities = cap,
         }
         lspconfig["tsserver"].setup {
             on_attach = on_attach,
--- lua/nahian/plugins/nvim-cmp.lua
+++ lua/nahian/plugins/nvim-cmp.lua
@@ -7,6 +7,7 @@ return {
 		{
 			"L3MON4D3/LuaSnip",
 			version = "v2.*",
+            build = "make install_jsregexp",
 			dependencies = { "rafamadriz/friendly-snippets" }, -- source for snippets
 		}, -- source snippet engine
 		"saadparwaiz1/cmp_luasnip", -- for autocomplete
