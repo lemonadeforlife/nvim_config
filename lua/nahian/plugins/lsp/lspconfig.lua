f1752bf4bf8eed6f6d7417ae79d49eeec83dad47
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2024-02-27 13:56:49 +0600

    Enhancement: Made lsp key binds globals

--- lua/nahian/plugins/lsp/lspconfig.lua
+++ lua/nahian/plugins/lsp/lspconfig.lua
@@ -10,28 +10,21 @@ return {
         require('neodev').setup {}
         local lspconfig = require('lspconfig')
         local cmp_nvim_lsp = require('cmp_nvim_lsp')
-
-        local on_attach = function(_,_)
-            local keymap = vim.keymap.set
-            --local opts = { noremap = true, silent = true }
-            keymap('n', '<leader>rn', vim.lsp.buf.rename, {})
-            keymap('n', '<leader>ca', vim.lsp.buf.code_action, {})
-
-            keymap('n', 'gd', vim.lsp.buf.definition, {})
-            keymap('n', 'gi', vim.lsp.buf.implementation, {})
-            keymap('n', 'gr', require("telescope.builtin").lsp_references, {})
-            keymap('n', 'K', vim.lsp.buf.hover, {})
-        end
+        local opts = { noremap = true, silent = true }
+        local keymap = vim.keymap
+        keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+        keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        keymap.set('n', 'gr', require("telescope.builtin").lsp_references, opts)
+        keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 
         -- used to enable autocompletion (assign to every lsp server config)
         local cap = cmp_nvim_lsp.default_capabilities()
         lspconfig["lua_ls"].setup {}
-        lspconfig["pyright"].setup {
-            on_attach = on_attach,
-            capabilities = cap,
-        }
+        -- lspconfig["pyright"].setup { capabilities = cap }
+        lspconfig["ruff_lsp"].setup {capabilities = cap }
         lspconfig["tsserver"].setup {
-            on_attach = on_attach,
             capabilities = cap,
             init_options = {
                 preferences = {
