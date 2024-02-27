8cc75ea60bbf360bcab65da840b5ce14ba0b1c30
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2024-02-27 13:45:23 +0600

    Implemented the lazy imports in store.lua

--- lua/nahian/store.lua
+++ lua/nahian/store.lua
@@ -10,3 +10,9 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup({
+	{ import = "nahian.plugins" },
+	{ import = "nahian.plugins.lsp" },
+})
