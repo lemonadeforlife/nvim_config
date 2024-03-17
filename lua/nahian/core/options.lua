7224560de089616c4f1cbf01a73c8df2c4e385d2
lemonadeforlife <nahianlabiblimon44@gmail.com>
2024-03-17 12:44:19 +0600

    Added Python Provider for NeoVim

--- lua/nahian/core/options.lua
+++ lua/nahian/core/options.lua
@@ -2,7 +2,10 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
+-- python
+vim.g.python_host_prog = "/home/nahian/.config/nvim/.nvim-venv/bin/python3"
 
+vim.g.python3_host_prog = "/home/nahian/.config/nvim/.nvim-venv/bin/python3"
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.conceallevel = 0 -- for `` visible in markdown
 vim.opt.ignorecase = true
