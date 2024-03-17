4c19060c4aea48fd4144ca2b512488516a3f36fb
lemonadeforlife <nahianlabiblimon44@gmail.com>
2024-03-17 11:27:15 +0600

    Separated VSCode feature from NeoVim

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
-require("nahian.core.options")
-require("nahian.store")
-require("lazy").setup({
-	{ import = "nahian.plugins" },
-	{ import = "nahian.plugins.lsp" },
-})
-require("nahian.core.keymaps")
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- Ordinary Extension
+    require("nahian.core.options")
+    require("nahian.store")
+    require("lazy").setup({
+        { import = "nahian.plugins" },
+        { import = "nahian.plugins.lsp" },
+    })
+    require("nahian.core.keymaps")
+end
