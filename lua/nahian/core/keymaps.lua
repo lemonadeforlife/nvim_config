2bf2c6752805a7947349e9143bb1bdff4143e5a0
lemonadeforlife <nahianlabiblimon44@gmail.com>
2024-03-17 23:15:56 +0600

    Added git hunk in keymaps

--- lua/nahian/core/keymaps.lua
+++ lua/nahian/core/keymaps.lua
@@ -5,7 +5,7 @@ local term_opts = { silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 local keymaps = vim.keymap.set
-local wk  = require("which-key")
+local wk = require("which-key")
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 
@@ -60,10 +60,9 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-keymaps({"n", "i", "v"}, "<C-p>", "<cmd>BufferLineTogglePin<CR>", opts)
+keymaps({ "n", "i", "v" }, "<C-p>", "<cmd>BufferLineTogglePin<CR>", opts)
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 keymap("n", "<leader>t", ":ToggleTerm direction=float<CR>", opts)
-
 
 -- ======================
 -- ##### which-key ######
@@ -71,69 +70,70 @@ keymap("n", "<leader>t", ":ToggleTerm direction=float<CR>", opts)
 
 -- No highlight
 wk.register({
-    ["<leader>h"] = { "<cmd>noh<CR>", "Remove highlight" }
+	["<leader>h"] = { "<cmd>noh<CR>", "Remove highlight" },
 })
 
 -- Quit NeoVim
 wk.register({
-    x = {
-        name = "Neovim",
-        a = { "<cmd>qa<cr>", "Quit all open files without saving" },
-        s = { "<cmd>w<cr>", "Save" },
-        x = { "<cmd>x<cr>", "Save & Exit"},
-        b = {"<cmd>bdelete!<cr>", "Close Buffer Window"},
-   }
+	x = {
+		name = "Neovim",
+		a = { "<cmd>qa<cr>", "Quit all open files without saving" },
+		s = { "<cmd>w<cr>", "Save" },
+		x = { "<cmd>x<cr>", "Save & Exit" },
+		b = { "<cmd>bdelete!<cr>", "Close Buffer Window" },
+	},
 }, { prefix = "<leader>" })
 
 -- bufferline
 wk.register({
-    b = {
-        name = "Buffers",
-        l = {"<cmd>BufferLineCloseRight<cr>", "Close Right Side Buffer"},
-        h = {"<cmd>BufferLineCloseLeft<cr>", "Close Left Side Buffer"},
-        f = {"<cmd>on<cr>", "Make the current window the only one on the screen"},
-    }
-}, {prefix="<leader>"})
+	b = {
+		name = "Buffers",
+		l = { "<cmd>BufferLineCloseRight<cr>", "Close Right Side Buffer" },
+		h = { "<cmd>BufferLineCloseLeft<cr>", "Close Left Side Buffer" },
+		f = { "<cmd>on<cr>", "Make the current window the only one on the screen" },
+	},
+}, { prefix = "<leader>" })
 
 -- NvimTreeToggle
-wk.register( { ["<leader>e"] = "File Explorer" } )
+wk.register({ ["<leader>e"] = "File Explorer" })
 
 -- ToggleTerm
-wk.register( { ["<leader>t"] = "Terminal" } )
+wk.register({ ["<leader>t"] = "Terminal" })
 
 -- Fold
 wk.register({
-    f = {
-        name = "fold",
-        c = {"<cmd>foldclose<cr>", "Fold Code"},
-        o = {"<cmd>foldopen<cr>", "Unfold Code"},
-    }
-}, {prefix="<leader>"})
+	f = {
+		name = "fold",
+		c = { "<cmd>foldclose<cr>", "Fold Code" },
+		o = { "<cmd>foldopen<cr>", "Unfold Code" },
+	},
+}, { prefix = "<leader>" })
 
 -- LSP Related Shortcuts: Trouble, Mason etc
 wk.register({
-    l = {
-        name = "LSP",
-        t = {"<cmd>TroubleToggle<cr>", "List of troubles from LSP"},
-        m = {"<cmd>Mason<cr>", "Mason Dashboard"},
-        n = {"<cmd>NullLsInfo<cr>", "Null Info Dashboard"},
-        i = {"<cmd>LspInfo<cr>", "Lsp Info Dashboard"},
-        l = {"<cmd>LspLog<cr>", "Lsp Log Dashboard"},
-        c = {"<cmd>CmpStatus<cr>", "Completion Status"},
-    }
-}, {prefix="<leader>"})
+	l = {
+		name = "LSP",
+		t = { "<cmd>TroubleToggle<cr>", "List of troubles from LSP" },
+		m = { "<cmd>Mason<cr>", "Mason Dashboard" },
+		n = { "<cmd>NullLsInfo<cr>", "Null Info Dashboard" },
+		i = { "<cmd>LspInfo<cr>", "Lsp Info Dashboard" },
+		l = { "<cmd>LspLog<cr>", "Lsp Log Dashboard" },
+		c = { "<cmd>CmpStatus<cr>", "Completion Status" },
+	},
+}, { prefix = "<leader>" })
 
--- telescope
+-- related to searching, git, finding files, telescope etc
 wk.register({
-    s = {
-        name = "Search",
-        f = {"<cmd>Telescope find_files<cr>", "Search for File for current working workspace"},
-        s = {"<cmd>Telescope live_grep<cr>", "Search for a string in your current working directory"},
-        h = {"<cmd>Telescope help_tags<cr>", "Returns List of relevant help tags for your"},
-        g = {"<cmd>Telescope git_files<cr>", "List Git files respecting .gitignore"},
-    },
-    g = {
-        name = "Git",
-        c = {"<cmd>Telescope git_commits<cr>", "List of Git commits"},
-    },
-}, {prefix="<leader>",silent=false, noremap=false})
+	s = {
+		name = "Search",
+		f = { "<cmd>Telescope find_files<cr>", "Search for File for current working workspace" },
+		s = { "<cmd>Telescope live_grep<cr>", "Search for a string in your current working directory" },
+		h = { "<cmd>Telescope help_tags<cr>", "Returns List of relevant help tags for your" },
+		g = { "<cmd>Telescope git_files<cr>", "List Git files respecting .gitignore" },
+	},
+	g = {
+		name = "Git",
+		c = { "<cmd>Telescope git_commits<cr>", "List of Git commits" },
+		p = { ":Gitsigns preview_hunk<cr>", "Shows you the changes on yellow bar" },
+	},
+}, { prefix = "<leader>", silent = false, noremap = false })
