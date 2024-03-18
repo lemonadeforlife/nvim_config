22e725b033b84bef334698758cec24cda661d17e
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2024-03-19 01:00:19 +0600

    Enhancement: redesign the whole neovim configuration
    The whole purpose of this redesign was for some reason nvim-cmp seems
    like not working for some lsp and facing some capabilities issue due to
    Lazy.nvim plugin manager.
    To avoid that we have separated the plugins and their configuration.
    
    Also rename the directory name "nahian" to "lemon"

---
+++ lua/lemon/lsp/mason.lua
@@ -0,0 +1,20 @@
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+local mason_auto = require("mason-tool-installer")
+mason.setup()
+mason_auto.setup({
+    ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "black",
+        "flake8",
+        "html",
+        "cssls",
+        "tsserver",
+        "eslint_d",
+        "prettier",
+    },
+})
+mason_lspconfig.setup({
+    automatic_installation = true,
+})
