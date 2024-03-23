dcf49724bf7ac9ff60c09bd7ab6d29cba9de7d36
lemonadeforlife <nahianlabiblimon44@gmail.com>
2024-03-24 01:20:50 +0600

    feature enhancement on shortcuts actions

--- lua/lemon/plugins/dashboard.lua
+++ lua/lemon/plugins/dashboard.lua
@@ -1,45 +1,53 @@
 return {
-    "nvimdev/dashboard-nvim",
-    event = "VimEnter",
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-        require("dashboard").setup {
-            theme = 'hyper',
-            config = {
-                week_header = {
-                    enable = true,
-                },
-                shortcut = {
-                    { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-                    {
-                      icon = ' ',
-                      icon_hl = '@variable',
-                      desc = 'Files',
-                      group = 'Label',
-                      action = 'Telescope find_files',
-                      key = 'f',
-                    },
-                    {
-                      desc = ' Code',
-                      group = 'DiagnosticHint',
-                      action = 'NvimTreeOpen ~/Code/',
-                      key = 'c',
-                    },
-                    {
-                      desc = ' dotfiles',
-                      action = 'Telescope find_files cwd=~/.config/nvim/',
-                      key = 'd',
-                    },
-                    {
-                        desc = '󰅜 Exit',
-                        group = 'Number',
-                        action = ':q',
-                        key = 'q',
-                    }
-                },
-            }
-        }
-    end
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("dashboard").setup({
+			theme = "hyper",
+			shortcut_type = "number",
+			config = {
+				week_header = {
+					enable = true,
+				},
+				shortcut = {
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+					{
+						icon = " ",
+						icon_hl = "@variable",
+						desc = "Files",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f",
+					},
+					{
+						desc = " Code",
+						group = "DiagnosticHint",
+						action = function()
+							vim.cmd([[cd ~/Code/]])
+							vim.cmd([[NvimTreeOpen ~/Code/]])
+						end,
+						key = "c",
+					},
+					{
+						desc = " dotfiles",
+						action = function()
+							vim.cmd([[cd ~/.config/nvim/]])
+							vim.cmd([[NvimTreeOpen ~/.config/nvim]])
+							vim.cmd([[Telescope find_files cwd=~/.config/nvim]])
+						end,
+						key = "d",
+					},
+					{
+						desc = "󰅜 Exit",
+						group = "Number",
+						action = ":q",
+						key = "q",
+					},
+				},
+			},
+		})
+	end,
 }
