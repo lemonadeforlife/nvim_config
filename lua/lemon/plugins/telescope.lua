6b29ab08109c1d2b9340b91daad24adc7f660a7e
lemonadeforlife <nahianlabiblimon44@gmail.com>
2024-03-24 01:32:00 +0600

    Feature enhancement on visuality & performance
    added suggested dependencies to improve it's functionality.
    also changed it's view to dropdown for easy code readability.

--- lua/lemon/plugins/telescope.lua
+++ lua/lemon/plugins/telescope.lua
@@ -1,7 +1,19 @@
 return {
-	'nvim-telescope/telescope.nvim', version = '0.1.4',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	version = "0.1.4",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	config = function()
-		require('telescope').setup {}
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					theme = "dropdown",
+				},
+			},
+		})
+		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("noice")
 	end,
 }
