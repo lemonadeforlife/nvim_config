d0820f375dbc8795ef8e0ad9775fead6aefc39e1
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2024-04-07 21:51:00 +0600

    updated telescope keymaps

--- lua/lemon/plugins/telescope.lua
+++ lua/lemon/plugins/telescope.lua
@@ -6,7 +6,22 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	config = function()
-		require("telescope").setup({
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		telescope.setup({
+			defaults = {
+				mappings = {
+					i = {
+						["<C-j>"] = actions.move_selection_next,
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+					},
+					n = {
+						["q"] = actions.close,
+						["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+					},
+				},
+			},
 			pickers = {
 				find_files = {
 					theme = "dropdown",
