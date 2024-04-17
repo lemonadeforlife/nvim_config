9857c1d6cd2d45dbd4a3b9eb096510237d74c4b0
lemonadeforlife <nahianlabiblimon44@gmail.com>
2024-04-17 16:16:01 +0600

    configured nvim-tree UI

--- lua/lemon/plugins/nvim_tree.lua
+++ lua/lemon/plugins/nvim_tree.lua
@@ -1,9 +1,24 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = {
-		"nvim-tree/nvim-web-devicons"
+		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-        require('nvim-tree').setup()
+		require("nvim-tree").setup({
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "󰳟",
+							arrow_open = "󰁆",
+						},
+					},
+				},
+			},
+		})
+		vim.cmd([[hi NvimTreeIndentMarker guifg=#73daca]])
 	end,
 }
