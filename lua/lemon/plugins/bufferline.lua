93de5fbc99b605649158c77e81b3a373b50bd981
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2024-04-25 16:16:16 +0600

    changed bufferline UI

--- lua/lemon/plugins/bufferline.lua
+++ lua/lemon/plugins/bufferline.lua
@@ -1,27 +1,27 @@
 return {
-	'akinsho/bufferline.nvim',
+	"akinsho/bufferline.nvim",
 	version = "*",
-	dependencies = 'nvim-tree/nvim-web-devicons',
+	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
-		require('bufferline').setup {
-            options = {
-                mode  = 'buffers',
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        highlight = "Directory",
-                        separator = true,
-                    }
-                },
-                separator_style = "slope",
-                hover = {
-                    enabled = true,
-                },
-                indicator = {
-                    style = 'underline',
-                },
-            }
-        }
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						highlight = "Directory",
+						separator = true,
+					},
+				},
+				separator_style = "thick",
+				hover = {
+					enabled = true,
+				},
+				indicator = {
+					style = "underline",
+				},
+			},
+		})
 	end,
 }
