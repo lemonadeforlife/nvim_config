52a12d38c379cdc3d7435f8210fc6e1af5750a3d
lemonadeforlife <61580921+lemonadeforlife@users.noreply.github.com>
2024-04-25 16:14:33 +0600

    added js diagnostics

--- lua/lemon/lsp/none_ls.lua
+++ lua/lemon/lsp/none_ls.lua
@@ -1,28 +1,32 @@
-local null_ls = require("null-ls")
-local formatting = null_ls.builtins.formatting
-local diagnos = null_ls.builtins.diagnostics
+local none_ls = require("null-ls")
+local formatting = none_ls.builtins.formatting
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-null_ls.setup({
-    sources = {
-        -- python
-        formatting.black,
-        require("none-ls.diagnostics.flake8"),
-        -- javascript
-        require("none-ls.diagnostics.eslint"),
-        formatting.prettier,
-        formatting.stylua,
-    },
-    -- format on save
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ async = false })
-                end,
-            })
-        end
-    end,
+none_ls.setup({
+	sources = {
+		-- python
+		formatting.black,
+		require("none-ls.diagnostics.flake8"),
+
+		-- Web Dev Formatting
+		formatting.prettier,
+
+		-- javascript
+    require("none-ls.diagnostics.eslint_d"),
+
+    -- lua formatting
+		formatting.stylua,
+	},
+	-- format on save
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	end,
 })
