82073581bc50ad3e48515451556a8f8bbef20f39
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-06-16 22:57:21 +0600

    added auto update for json

--- lua/lemon/lsp/mason.lua
+++ lua/lemon/lsp/mason.lua
@@ -3,7 +3,6 @@ local mason_lspconfig = require("mason-lspconfig")
 local mason_auto = require("mason-tool-installer")
 mason.setup()
 mason_auto.setup({
-<<<<<<< HEAD
   ensure_installed = {
     "lua_ls",
     -- bash/zsh
