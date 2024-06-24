b5897918abf7f063ac29836c4f03144b6222dd49
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-06-24 11:30:42 +0600

    added entry for nvim-vscode

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
-require("lemon.core.options")
-require("lemon.store")
-require("lazy").setup({ import = "lemon.plugins" })
-require("lemon.lsp")
-require("lemon.core.keymaps")
+if vim.g.vscode then
+	require("lemon.core.vsoptions")
+else
+	require("lemon.core.options")
+	require("lemon.store")
+	require("lazy").setup({ import = "lemon.plugins" })
+	require("lemon.lsp")
+	require("lemon.core.keymaps")
+end
