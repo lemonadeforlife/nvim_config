339a968e5b39d0fc33140842b92e5c08273676da
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-06-26 11:22:50 +0600

    always show buffer to avoid nvim tree closing error

--- lua/lemon/plugins/bufferline.lua
+++ lua/lemon/plugins/bufferline.lua
@@ -1,28 +1,31 @@
 return {
-	"akinsho/bufferline.nvim",
-	branch = "main",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		require("bufferline").setup({
-			options = {
-				mode = "buffers",
-				offsets = {
-					{
-						filetype = "NvimTree",
-						text = "File Explorer",
-						highlight = "Directory",
-						separator = true,
-					},
-				},
-				separator_style = "thick",
-				hover = {
-					enabled = true,
-				},
-				indicator = {
-					style = "underline",
-				},
-			},
-		})
-	end,
+  "akinsho/bufferline.nvim",
+  branch = "main",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("bufferline").setup({
+      options = {
+        mode = "buffers",
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+            separator = true,
+          },
+        },
+        separator_style = "thick",
+        hover = {
+          enabled = true,
+        },
+        indicator = {
+          style = "underline",
+        },
+        show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+        always_show_bufferline = true,
+        auto_toggle_bufferline = true,
+      },
+    })
+  end,
 }
