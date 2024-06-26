db13e57852b6ec23e642b9f1988974a80f4cc6b7
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-06-26 10:53:35 +0600

    added stylua

--- lua/lemon/lsp/mason.lua
+++ lua/lemon/lsp/mason.lua
@@ -3,29 +3,30 @@ local mason_lspconfig = require("mason-lspconfig")
 local mason_auto = require("mason-tool-installer")
 mason.setup()
 mason_auto.setup({
-	ensure_installed = {
-		"lua_ls",
-		-- bash/zsh
-		"bashls",
-		"beautysh",
-		"shellcheck",
-		-- C/C++
-		"clangd",
-		"clang-format",
-		-- python
-		"pyright",
-		"black",
-		"flake8",
-		-- web dev stuff
-		"html",
-		"cssls",
-		"tsserver",
-		"prettierd",
-		-- json
-		"jsonls",
-	},
-	auto_update = true,
+  ensure_installed = {
+    "lua_ls",
+    "stylua",
+    -- bash/zsh
+    "bashls",
+    "beautysh",
+    "shellcheck",
+    -- C/C++
+    "clangd",
+    "clang-format",
+    -- python
+    "pyright",
+    "black",
+    "flake8",
+    -- web dev stuff
+    "html",
+    "cssls",
+    "tsserver",
+    "prettierd",
+    -- json
+    "jsonls",
+  },
+  auto_update = true,
 })
 mason_lspconfig.setup({
-	automatic_installation = true,
+  automatic_installation = true,
 })
