73862113094b9eab96c1c21f93325814404845de
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-10-14 18:34:12 +0600

    fixed trouble.lua & changed layout toggle_term

--- lua/lemon/plugins/toggle_term.lua
+++ lua/lemon/plugins/toggle_term.lua
@@ -1,5 +1,18 @@
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
-	config = true,
+	config = function()
+		require("toggleterm").setup({
+			size = function(term)
+				if term.direction == "vertical" then
+					return vim.o.columns * 0.4142
+				else
+					if term.direction == "horizontal" then
+						return vim.o.lines * 0.35
+					end
+				end
+			end,
+			start_in_insert = true,
+		})
+	end,
 }
