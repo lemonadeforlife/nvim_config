807daaf68c1d0386d6d8fbc9135420c9cdf32a59
Lemonade Win <nlsource22811@gmail.com>
2024-10-14 18:16:26 +0600

    added windows support for neovim

--- lua/lemon/lsp/lspconfig.lua
+++ lua/lemon/lsp/lspconfig.lua
@@ -4,65 +4,65 @@ local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 -- attaching keymaps for lsp's
 local on_attach = function(_, _)
-	local telescope = require("telescope.builtin")
-	local keymap = vim.keymap.set
-	--local opts = { noremap = true, silent = true }
-	keymap("n", "<leader>rn", vim.lsp.buf.rename, {})
-	keymap("n", "<leader>ca", vim.lsp.buf.code_action, {})
+  local telescope = require("telescope.builtin")
+  local keymap = vim.keymap.set
+  --local opts = { noremap = true, silent = true }
+  keymap("n", "<leader>rn", vim.lsp.buf.rename, {})
+  keymap("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-	keymap("n", "gd", telescope.lsp_definitions, {})
-	keymap("n", "gi", telescope.lsp_implementations, {})
-	keymap("n", "gr", telescope.lsp_references, {})
-	keymap("n", "K", vim.lsp.buf.hover, {})
+  keymap("n", "gd", telescope.lsp_definitions, {})
+  keymap("n", "gi", telescope.lsp_implementations, {})
+  keymap("n", "gr", telescope.lsp_references, {})
+  keymap("n", "K", vim.lsp.buf.hover, {})
 end
 
 -- declare client to show diagnostics in insert mode as well
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	-- delay update diagnostics
-	update_in_insert = true,
+  -- delay update diagnostics
+  update_in_insert = true,
 })
 -- used to enable autocompletion (assign to every lsp server config)
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- lsp configs
 lspconfig["lua_ls"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 })
 lspconfig["bashls"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "sh", "zsh" },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "sh", "zsh" },
 })
 lspconfig["html"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	init_options = {
-		configurationSection = { "html", "css", "javascript" },
-		embeddedLanguages = {
-			css = true,
-			javascript = true,
-		},
-		provideFormatter = true,
-	},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    configurationSection = { "html", "css", "javascript" },
+    embeddedLanguages = {
+      css = true,
+      javascript = true,
+    },
+    provideFormatter = true,
+  },
 })
 lspconfig["cssls"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	init_options = {
-		provideFormatter = true,
-	},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    provideFormatter = true,
+  },
 })
 lspconfig["pyright"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+  on_attach = on_attach,
+  capabilities = capabilities,
 })
-lspconfig["tsserver"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	init_options = {
-		preferences = {
-			disableSuggestions = true,
-		},
-	},
+lspconfig["ts_ls"].setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  init_options = {
+    preferences = {
+      disableSuggestions = true,
+    },
+  },
 })
--- lua/lemon/plugins/telescope.lua
+++ lua/lemon/plugins/telescope.lua
@@ -1,34 +1,42 @@
+local function buildCmd()
+  local os_name = vim.loop.os_uname().sysname
+  if os_name == "Linux" then
+    return "make"
+  else
+    return "gcc"
+  end
+end
 return {
-	"nvim-telescope/telescope.nvim",
-	version = "0.1.4",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
-		telescope.setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<C-j>"] = actions.move_selection_next,
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-p>"] = require("telescope.actions.layout").toggle_preview,
-					},
-					n = {
-						["q"] = actions.close,
-						["<C-p>"] = require("telescope.actions.layout").toggle_preview,
-					},
-				},
-			},
-			pickers = {
-				find_files = {
-					theme = "dropdown",
-				},
-			},
-		})
-		require("telescope").load_extension("fzf")
-		require("telescope").load_extension("noice")
-	end,
+  "nvim-telescope/telescope.nvim",
+  version = "0.1.4",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = buildCmd() },
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+          },
+          n = {
+            ["q"] = actions.close,
+            ["<C-p>"] = require("telescope.actions.layout").toggle_preview,
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          theme = "dropdown",
+        },
+      },
+    })
+    require("telescope").load_extension("fzf")
+    require("telescope").load_extension("noice")
+  end,
 }
