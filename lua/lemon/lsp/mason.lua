b3e67dcc79e3edb1adedc8a641e15ee07e4a32ce
Lemonade Win <61580921+lemonadeforlife@users.noreply.github.com>
2024-10-14 18:16:26 +0600

    added windows support for neovim

--- lua/lemon/lsp/mason.lua
+++ lua/lemon/lsp/mason.lua
@@ -3,25 +3,25 @@ local mason_lspconfig = require("mason-lspconfig")
 local mason_auto = require("mason-tool-installer")
 mason.setup()
 mason_auto.setup({
-	ensure_installed = {
-		"lua_ls",
-		-- bash/zsh
-		"bashls",
-		"beautysh",
-		"shellcheck",
-		-- python
-		"pyright",
-		"black",
-		"flake8",
-		-- web dev stuff
-		"html",
-		"cssls",
-		"tsserver",
-		"eslint",
-		"prettier",
-	},
-	auto_update = true,
+  ensure_installed = {
+    "lua_ls",
+    -- bash/zsh
+    "bashls",
+    "beautysh",
+    "shellcheck",
+    -- python
+    "pyright",
+    "black",
+    "flake8",
+    -- web dev stuff
+    "html",
+    "cssls",
+    "ts_ls",
+    "eslint",
+    "prettier",
+  },
+  auto_update = true,
 })
 mason_lspconfig.setup({
-	automatic_installation = true,
+  automatic_installation = true,
 })
--- lua/lemon/plugins/which_key.lua
+++ lua/lemon/plugins/which_key.lua
@@ -1,11 +1,14 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	config = function()
-		require("which-key").setup({})
-	end,
+  "folke/which-key.nvim",
+  dependencies = {
+    { 'echasnovski/mini.nvim', version = '*' },
+  },
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  config = function()
+    require("which-key").setup({})
+  end,
 }
