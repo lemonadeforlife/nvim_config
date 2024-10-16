ee69beaf38593144a6400f9756ebef5e56bacc74
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-10-16 17:03:29 +0600

    made neovim natively compatbiles
    Telescope native support
    C/C++ native support

--- lua/lemon/lsp/mason.lua
+++ lua/lemon/lsp/mason.lua
@@ -1,33 +1,38 @@
 local mason = require("mason")
 local mason_lspconfig = require("mason-lspconfig")
 local mason_auto = require("mason-tool-installer")
+local os_name = vim.loop.os_uname().sysname
+local package_list = {
+	"lua_ls",
+	"stylua",
+	-- bash/zsh
+	"bashls",
+	"beautysh",
+	"shellcheck",
+	-- C/C++
+	"clang-format",
+	-- python
+	"pyright",
+	"black",
+	"flake8",
+	-- web dev stuff
+	"html",
+	"cssls",
+	"emmet_language_server",
+	"ts_ls",
+	"prettierd",
+	-- json
+	"jsonls",
+}
+if os_name == "Windows_NT" then
+	table.remove(package_list, 6)
+end
+
 mason.setup()
 mason_auto.setup({
-	ensure_installed = {
-		"lua_ls",
-		"stylua",
-		-- bash/zsh
-		"bashls",
-		"beautysh",
-		"shellcheck",
-		-- C/C++
-		"clangd",
-		"clang-format",
-		-- python
-		"pyright",
-		"black",
-		"flake8",
-		-- web dev stuff
-		"html",
-		"cssls",
-		"emmet_language_server",
-		"ts_ls",
-		"prettierd",
-		-- json
-		"jsonls",
-	},
+	ensure_installed = package_list,
 	auto_update = true,
 })
 mason_lspconfig.setup({
-  automatic_installation = true,
+	automatic_installation = false,
 })
