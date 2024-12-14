6589d7ea2d40af6074a9e95bb567c50bf1018a59
lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-12-14 15:42:25 +0600

    Added dracula theme in neovim

--- lua/lemon/plugins/lualine.lua
+++ lua/lemon/plugins/lualine.lua
@@ -1,33 +1,33 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "tokyonight",
-        component_separators = "",
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = { statusline = { 'NvimTree' } }
-      },
-      globalstatus = true,
-      sections = {
-        lualine_a = { { "mode", separator = { left = "", right = "" }, right_padding = 2 } },
-        lualine_b = { "branch" },
-        lualine_c = { require("auto-session.lib").current_session_name },
-        lualine_x = {},
-        lualine_y = { "filetype", "progress" },
-        lualine_z = {
-          { "location", separator = { right = "" }, left_padding = 2 },
-        },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "mode" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+	config = function()
+		require("lualine").setup({
+			options = {
+				-- theme = "tokyonight",
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = { statusline = { "NvimTree" } },
+			},
+			globalstatus = true,
+			sections = {
+				lualine_a = { { "mode", separator = { left = "", right = "" }, right_padding = 2 } },
+				lualine_b = { "branch" },
+				lualine_c = { require("auto-session.lib").current_session_name },
+				lualine_x = {},
+				lualine_y = { "filetype", "progress" },
+				lualine_z = {
+					{ "location", separator = { right = "" }, left_padding = 2 },
+				},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "mode" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+	end,
 }
