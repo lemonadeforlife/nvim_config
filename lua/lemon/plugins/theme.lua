43bbeea2ddff55c5e64bf35e5442cf93783192c9
lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2024-12-17 16:49:20 +0600

    Added tokyonight dracula catppuccin themes

--- lua/lemon/plugins/theme.lua
+++ lua/lemon/plugins/theme.lua
@@ -1,17 +1,34 @@
 return {
-	-- "folke/tokyonight.nvim",
-	-- config = function()
-	-- 	require("tokyonight").setup({
-	-- 		style = "moon",
-	-- 		transparent = false,
-	-- 		dim_inactive = true,
-	-- 		lualine_bold = true,
-	-- 	})
-	-- end,
-	"dracula/vim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd([[colorscheme dracula]])
-	end,
+	-- enter your theme repo
+
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				transparent = false,
+				dim_inactive = true,
+				lualine_bold = true,
+			})
+			vim.cmd([[colorscheme tokyonight-night]])
+		end,
+	},
+	{
+		"dracula/vim",
+		lazy = false,
+		priority = 1000,
+		-- config = function()
+		--   vim.cmd([[colorscheme dracula]])
+		-- end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		-- config = function()
+		--   vim.cmd([[colorscheme catppuccin-mocha]])
+		-- end,
+	},
 }
