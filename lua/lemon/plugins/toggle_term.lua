25a25e5035982dacc9ef1730c8b2175636524d8b
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2025-02-01 20:17:09 +0600

    added powershell as default toggleterm shell only for windows

--- lua/lemon/plugins/toggle_term.lua
+++ lua/lemon/plugins/toggle_term.lua
@@ -2,6 +2,21 @@ return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
 	config = function()
+		-- source: https://github.com/akinsho/toggleterm.nvim/wiki/Tips-and-Tricks#using-toggleterm-with-powershell
+		-- All of the following options have to be set before require("toggleterm").setup{} is called or they won't have any effect.
+		if os_name == "Windows_NT" then
+			local powershell_options = {
+				shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+				shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+				shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+				shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+				shellquote = "",
+				shellxquote = "",
+			}
+			for option, value in pairs(powershell_options) do
+				vim.opt[option] = value
+			end
+		end
 		require("toggleterm").setup({
 			size = function(term)
 				if term.direction == "vertical" then
