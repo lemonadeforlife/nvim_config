5e9811511084f55071c8c38b7f8dc82baac35d29
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2025-02-01 20:18:30 +0600

    renamed theme.lua to themes.lua made it plural for obvious reasons

---
+++ lua/lemon/plugins/themes.lua
@@ -0,0 +1,34 @@
+return {
+	-- enter your theme repo
+
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("tokyonight").setup({
+				transparent = false,
+				dim_inactive = true,
+				lualine_bold = true,
+			})
+			-- vim.cmd([[colorscheme tokyonight-night]])
+		end,
+	},
+	{
+		"dracula/vim",
+		lazy = false,
+		priority = 1000,
+		-- config = function()
+		--   vim.cmd([[colorscheme dracula]])
+		-- end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		-- config = function()
+		--   vim.cmd([[colorscheme catppuccin-mocha]])
+		-- end,
+	},
+}
