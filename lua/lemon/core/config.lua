6e080eea9f017e640354e70e1a439f4049ace98d
Lemonade <61580921+lemonadeforlife@users.noreply.github.com>
2025-02-17 16:29:11 +0600

    Updated themes for windows

--- lua/lemon/core/config.lua
+++ lua/lemon/core/config.lua
@@ -2,7 +2,11 @@
 if os_name == "Linux" then
 	vim.cmd([[colorscheme tokyonight-night]])
 elseif os_name == "Windows_NT" then
-	vim.cmd([[colorscheme dracula]])
+	require("catppuccin").setup({
+		flavour = "mocha",
+		transparent_background = true,
+	})
+	vim.cmd.colorscheme("catppuccin")
 else
 	vim.cmd([[colorscheme catppuccin-mocha]])
 end
